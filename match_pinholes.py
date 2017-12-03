da0ca33dbcaac8f747606564c6d11c6da4eacb93
mwanakijiji <spalding@email.arizona.edu>
2017-12-03 16:07:27 -0700

    Matching of model and empirical pinholes works

--- match_pinholes.py
+++ match_pinholes.py
@@ -28,14 +28,19 @@ write_hash('git_hash_match_pinholes.txt')
 hdul = fits.open(calibrated_pinholes_data_stem+'step02_medianed/pinhole_image_median_DXonly_171108.fits') # median image of pinholes, with vignetted regions masked
 imagePinholes = hdul[0].data.copy()
 
-xCoordsIdealFullGrid, yCoordsIdealFullGrid = find_pinhole_centroids.put_down_grid_guesses([100,-100],48.0,[512,512],2e-8,0.65) # sets down an 'ideal' set of pinholes made to match the real pinholes as closely as possible
+xCoordsIdealFullGrid, yCoordsIdealFullGrid = find_pinhole_centroids.put_down_grid_guesses([110,-110],48.0,[100,512],-1e-9,-3.2) # sets down an 'ideal' set of pinholes made to match the real pinholes as closely as possible
 # approxHoleSpacingPass,barrelCenterPass,barrelDegreePass,rotationAnglePass
+
 import ipdb; ipdb.set_trace()
-xCoordsFoundAutomated, yCoordsFoundAutomated = find_pinhole_centroids.find_psf_centers(imagePinholes,20.,50000.) # finds the actual st of pinholes
+xCoordsFoundAutomated, yCoordsFoundAutomated = find_pinhole_centroids.find_psf_centers(imagePinholes,20.,30000.) # finds the actual st of pinholes
 
 # manually-found locations of pinholes that the above routine missed
-xCoordsMissed = [71.774,697.353,1460.66]
-yCoordsMissed = [1267.57,1404.06,737.932]
+##xCoordsMissed = [71.774,697.353,1460.66]
+##yCoordsMissed = [1267.57,1404.06,737.932]
+
+# connect the automated pinhole locations with counterparts in the model grid
+
+
 
 # remove subset of pinhole guesses that are in the vignetted region
 xCoordsIdeal = xCoordsIdealFullGrid[np.where(np.logical_or(xCoordsIdealFullGrid>36,
