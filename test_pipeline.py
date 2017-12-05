8351010d277bf95c5b4b476fa76adb9a2513fd27
mwanakijiji <spalding@email.arizona.edu>
2017-12-05 12:16:27 -0700

    Just before changing nearest-neighbor code for barb plot

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,8 +2,16 @@
 
 # created by E.S., 2017 Nov 11
 
-from astrom import match_pinholes # high-level import of needed methods and functions
+from astrom import match_pinholes
+from astrom import find_dewarp_solution
 
-match_pinholes.match_pinholes(plot=False) # do the reduction
+match_pinholes.match_pinholes(
+    translation=[110,-110],
+    holeSpacing=48.0,
+    barrelCenter=[100,512],
+    barrelAmount=-1e-9,
+    rotationAngle=-3.2,
+    plot=False) # do the reduction
 
+find_dewarp_solution()
 
