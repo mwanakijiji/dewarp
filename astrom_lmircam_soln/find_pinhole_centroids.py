84e1dac823a6422661119e2a2d3c72cdbbc57256
mwanakijiji <spalding@email.arizona.edu>
2017-12-04 19:59:53 -0700

    Matching pinholes section working

--- astrom_lmircam_soln/find_pinhole_centroids.py
+++ astrom_lmircam_soln/find_pinhole_centroids.py
@@ -9,6 +9,7 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import photutils
 from photutils import DAOStarFinder
+from scipy import spatial
 
 def rotation_matrix(angleDegPass): # use degrees in argument
     radEquiv = angleDegPass*np.pi/180.
@@ -32,6 +33,8 @@ def put_down_grid_guesses(displacementPass,approxHoleSpacingPass,barrelCenterPas
     xHoles = np.arange(0.0, 2048.0, approxHoleSpacingPass)
     yHoles = np.arange(0.0, 2048.0, approxHoleSpacingPass)
     xHolesMeshGrid, yHolesMeshGrid = np.meshgrid(xHoles,yHoles) # arrange into an (x,y) grid
+    xHolesMeshGrid_no_d = np.copy(xHolesMeshGrid) # make copy for undistorted coordinates
+    yHolesMeshGrid_no_d = np.copy(yHolesMeshGrid)
     
     # add barrel distortion: radial distance from a point (x0,y0) is r_d = r_u*(1-alpha*|r_u|**2) 
     xHolesMeshGrid_d, yHolesMeshGrid_d = np.multiply(
@@ -48,21 +51,85 @@ def put_down_grid_guesses(displacementPass,approxHoleSpacingPass,barrelCenterPas
     # add (x0,y0) back in, as well as the desired translation
     xHolesMeshGrid_d += barrelCenterPass[0] + displacementPass[0]
     yHolesMeshGrid_d += barrelCenterPass[1] + displacementPass[1]
+    xHolesMeshGrid_no_d += displacementPass[0] # note barrelCenterPass was not subtracted out
+    yHolesMeshGrid_no_d += displacementPass[1]
 
     # rotate
-    coordMatrix = np.matrix(np.transpose([np.ravel(xHolesMeshGrid_d),np.ravel(yHolesMeshGrid_d)]))
-    xHolesMeshGrid_d_r = (np.matmul(coordMatrix,rotation_matrix(rotationAnglePass)))[:,0]
-    yHolesMeshGrid_d_r = (np.matmul(coordMatrix,rotation_matrix(rotationAnglePass)))[:,1]
+    coordMatrix_d = np.matrix(np.transpose([np.ravel(xHolesMeshGrid_d),np.ravel(yHolesMeshGrid_d)]))
+    coordMatrix_no_d = np.matrix(np.transpose([np.ravel(xHolesMeshGrid_no_d),np.ravel(yHolesMeshGrid_no_d)]))
+    xHolesMeshGrid_d_r = (np.matmul(coordMatrix_d,rotation_matrix(rotationAnglePass)))[:,0]
+    yHolesMeshGrid_d_r = (np.matmul(coordMatrix_d,rotation_matrix(rotationAnglePass)))[:,1]
+    xHolesMeshGrid_no_d_r = (np.matmul(coordMatrix_no_d,rotation_matrix(rotationAnglePass)))[:,0]
+    yHolesMeshGrid_no_d_r = (np.matmul(coordMatrix_no_d,rotation_matrix(rotationAnglePass)))[:,1]
 
-    return xHolesMeshGrid_d_r, yHolesMeshGrid_d_r
+    ptsModel_d_Pass = np.concatenate((xHolesMeshGrid_d_r,yHolesMeshGrid_d_r),axis=1)
+    ptsModel_no_d_Pass = np.concatenate((xHolesMeshGrid_no_d_r,yHolesMeshGrid_no_d_r),axis=1)
+
+    # return distorted [x,y], undistorted [x,y]
+    return ptsModel_d_Pass, ptsModel_no_d_Pass
 
 def find_psf_centers(imagePass,fwhmPass,thresholdPass):
-    # uses the grid of guesses to find the real centroids
-    
+    '''
+    employ DAOPhot to find PSF coordinates
+    '''
     daofind = DAOStarFinder(fwhm=fwhmPass, threshold=thresholdPass, exclude_border=True)
     sources = daofind(imagePass)
 
-    return sources['xcentroid'], sources['ycentroid']
+    # put the x,y coordinates into an array
+    ptsEmpiricalPass = np.transpose(np.concatenate(([sources['xcentroid']], [sources['ycentroid']]),axis=0))
+
+    return ptsEmpiricalPass
+
+
+def match_model_empirical(
+        ptsModel_d_Pass,
+        ptsModel_not_d_Pass,
+        ptsEmpiricalPass,
+        imagePinholesPass,
+        plot=True):
+    '''
+    matches nearest neighbors between model and empirical pinhole locations
+    INPUTS:
+    ptsModel_d_Pass: the pinhole locations in the distorted model; needed for matching empirical pinholes on a nearest-neighbor basis (doesn't need to be sorted, but should be larger than ptsEmpiricalPass to make use of as many pinholes as possible)
+    ptsModel_not_d_Pass: same as ptsModel_d_Pass, except free of distortion; needed to underpin dewarp solution
+    ptsEmpiricalPass: the pinhole locations found by DAOPhot (doesn't need to be sorted)
+    plot: display a plot as a check?
+    '''
+
+    treeModel = spatial.KDTree(ptsModel_d_Pass) # set up tree to query
+    indicesOfInterest = treeModel.query(ptsEmpiricalPass) # find nearest neighbors; indicesOfInterest[1][:] are array indices
+    xEmpirical = ptsEmpiricalPass[:,0] # rename
+    yEmpirical = ptsEmpiricalPass[:,1]
+    xModel_d = ptsModel_d_Pass[indicesOfInterest[1][:]][:,0] # sort 
+    yModel_d = ptsModel_d_Pass[indicesOfInterest[1][:]][:,1]
+    xModel_not_d = ptsModel_not_d_Pass[indicesOfInterest[1][:]][:,0] # apply same sorted indices as before to undistorted model
+    yModel_not_d = ptsModel_not_d_Pass[indicesOfInterest[1][:]][:,1]
+
+    if plot: # make a plot to check mapping between coordinates
+        
+        # image background
+        plt.imshow(imagePinholesPass, origin="lower", cmap="gray")
+        
+        # plot coordinates
+        plt.scatter(xEmpirical,yEmpirical,color="red")
+        plt.scatter(xModel_d,yModel_d,color="yellow") # distorted
+        plt.scatter(xModel_not_d,yModel_not_d,color="blue") # not distorted
+        
+        # list comprehension for drawing white lines between model and empirical points
+        [plt.plot([xEmpirical[j],xModel_not_d[j]],[yEmpirical[j],yModel_not_d[j]], color="w") for j in range(len(xEmpirical))]
+
+        plt.title('red = empirical; yellow = distorted model; blue = undistorted model')
+        plt.xlabel('x (pix)')
+        plt.ylabel('y (pix)')
+        plt.show()
+
+    # package everything
+    coords_d_Pass = np.transpose(np.squeeze([xModel_d,yModel_d]))
+    coords_not_d_Pass = np.transpose(np.squeeze([xModel_not_d,yModel_not_d]))
+    coordsEmpiricalPass = np.transpose([xEmpirical,yEmpirical])
+
+    return coords_d_Pass, coords_not_d_Pass, coordsEmpiricalPass
+    
 
 def consistency_xy_found_guesses(xCoordsGuessesPass,yCoordsGuessesPass,xCoordsFoundPass,yCoordsFoundPass):
     # makes each of the entries in the x,y lists of the guessed and found pinholes refer to the same pinholes
---
+++ test_pipeline.py
@@ -0,0 +1,9 @@
+# This is the script that should run the entire pipeline to reduce the delta Cyg data from Feb 2017
+
+# created by E.S., 2017 Nov 11
+
+from astrom import match_pinholes # high-level import of needed methods and functions
+
+match_pinholes.match_pinholes(plot=False) # do the reduction
+
+
